51dad0f15af6755f0714f0125b21e3a9d36295f2
Dan Dudley <dan.w.dudley@gmail.com>
2018-03-22 19:54:22 -0400

    Proof of concept testing : Read / Parse XML

--- 5KToolKitConsole/Program.cs
+++ 5KToolKitConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace _5KToolKitConsole
 {
@@ -10,6 +11,80 @@ namespace _5KToolKitConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("======================");
+            Console.WriteLine("This is a debug window");
+            Console.WriteLine("======================");
+            ParseExample();
+            Console.WriteLine("End of Book Sample");
+            L5XExample();
+            Console.WriteLine("End of L5X exampl");
+            Console.ReadLine();
+        }
+
+        // Example - Open and Read XML
+        // From - https://www.c-sharpcorner.com/UploadFile/dhananjaycoder/reading-xml-file-through-linq-a-few-tips/
+        public static void ParseExample()
+        {
+            XDocument document = XDocument.Load("C:\\Users\\danie\\source\\repos\\5KToolKit\\Examples\\sample.xml");
+            var books = from r in document.Descendants("book")
+                select new
+                {
+                    Author = r.Element("author").Value,
+                    Title = r.Element("title").Value,
+                    Genere = r.Element("genre").Value,
+                    Price = r.Element("price").Value,
+                    PublishDate = r.Element("publish_date").Value,
+                    Description = r.Element("description").Value,
+                };
+
+            foreach (var r in books)
+            {
+                Console.WriteLine(r.PublishDate + r.Title + r.Author);
+            }
+
+            Console.ReadKey(true);
+
+            var selectedBook = from r in document.Descendants("book").Where
+                                   (r => (string)r.Attribute("id") == "bk102")
+                               select new
+                               {
+                                   Author = r.Element("author").Value,
+                                   Title = r.Element("title").Value,
+                                   Genere = r.Element("genre").Value,
+                                   Price = r.Element("price").Value,
+                                   PublishDate = r.Element("publish_date").Value,
+                                   Description = r.Element("description").Value,
+
+                               };
+
+            foreach (var r in selectedBook)
+            {
+                Console.WriteLine(r.PublishDate + r.Title + r.Author);
+            }
+            Console.ReadKey(true);
+
+            var selectedBookAttribute = (from r in document.Descendants("book").Where
+                                        (r => (string)r.Attribute("id") == "bk102")
+                                         select r.Element("author").Attribute("id").Value).FirstOrDefault();
+
+            Console.WriteLine(selectedBookAttribute);
+
+            Console.ReadKey(true);
+        
+            var allauthors = from r in document.Descendants("book")
+                             select r.Element("author").Value;
+            foreach (var r in allauthors)
+            {
+                Console.WriteLine(r.ToString());
+            }
+
+            Console.ReadKey(true);
+
+        }
+
+        public static void L5XExample()
+        {
+
         }
     }
 }
